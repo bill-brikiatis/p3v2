2de8fdfa0eaacee75fba2472879cf46ba01cde21
bill-brikiatis <brikiatis@g.harvard.edu>
2014-07-13 20:03:11 -0400

    Created Text Generator.

--- app/views/gentext.blade.php
+++ app/views/gentext.blade.php
@@ -1,3 +1,26 @@
+<?php
+	class Gen {
+		var $graphs;
+		
+		public function __construct($graphs) {
+			$this->graphs = $graphs;
+		}
+		
+		public function genText() {
+			if($this->graphs) {
+				$generator = new Badcow\LoremIpsum\Generator();
+				$paragraphs = $generator->getParagraphs($this->graphs);
+				echo implode('<p>', $paragraphs);
+			}
+			else {
+				echo "Something's wrong. There are no paragraphs indicated.";
+			}
+			
+		}
+		
+	}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,13 +28,40 @@
 	<title>Lorem Ipsum Text Generator</title>
 </head>
 <body>
-	<div class="welcome">
-		<h1>Lorem Ipsum Text Generator</h1>
+	<h1>Lorem Ipsum Text Generator</h1>
+	
+	{{ Form::open(array( 
+		'url' => 'gentext', 
+		'method' => 'GET'
+		)) }}
+		
+		{{ Form::label('graphs', '# of Paragraphs') }}
+		{{ Form::select('graphs', array(
+			1 => 1,
+			2 => 2,
+			3 => 3,
+			4 => 4,
+			5 => 5,
+			6 => 6,
+			7 => 7,
+			8 => 8,
+			9 => 9,
+			10 => 10
+		
+		), 1) }}
+		
+	{{ Form::submit('Submit') }}
+		
+	{{ Form::close() }}
+
+	<div class="gen">
+		<?php $graphs = $_GET['graphs']; ?>
+		<?php $obj1 = new Gen($graphs); ?>
+		{{ $obj1->genText() }}
 	</div>
-	<?php
-		$generator = new Badcow\LoremIpsum\Generator();
-		$paragraphs = $generator->getParagraphs(1);
-		echo implode('<p>', $paragraphs);
-	?>
 </body>
 </html>
+
+
+
+
