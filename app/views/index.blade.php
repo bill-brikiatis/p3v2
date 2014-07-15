f77c1c9aa903634fdb9059060c56598e29d758eb
bill-brikiatis <brikiatis@g.harvard.edu>
2014-07-15 09:59:41 -0400

    Revised index page, fix typo.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -11,15 +11,16 @@
 	<h2>Lorem Ipsum Generator</h2>
 	
 	<p>In Web development, content is often created simultaneously with a website. This puts the developer in the position of having to
-	create and test the site without having even a draft of the content. The placeholder content that developers use is often called 
-	Lorem Ipsum because, in the past, Latin was used for placeholder content. This generator creates placeholder text for developers.</p>
+	create and test the site without having even a draft of the content. To deal with this situation, developers use placeholder content.
+	The placeholder content that developers use is often called Lorem Ipsum because, in the past, Latin was used. This generator creates 
+	placeholder text for developers.</p>
 	
 	<a href="gentext">Generate text ...</a>
 	
 	<h2>Dummy User Data</h2>
 	
 	<p>Like Lorem Ipsum, developers frequently need to populate systems with dummy user data for demonstration and testing. This 
-	Generator creates dummy user data for developers to use in applications.</p>
+	generator creates dummy user data for developers to use in applications.</p>
 	
 	<a href="user-data">Generate dummy user data ...</a>
 
